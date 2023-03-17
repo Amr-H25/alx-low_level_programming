b3e8fea8ce6708b877b74ecabe50beee4f5c02bf
Amr-H25 <amrhesham042@gmail.com>
2023-03-16 18:22:02 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  *     *    ssize_t write(int fd, const void *buf, size_t count);
  *      *
  *       * Return: 1 (error).
- *       */
-
-
+ */
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
