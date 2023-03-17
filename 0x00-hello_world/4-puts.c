a11f0cc8701d347b229129e61c5a90ec63ac8c98
Amr-H25 <amrhesham042@gmail.com>
2023-03-16 17:46:07 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints with print functuin
+ *
+ * Return: ALways 0 (Success)
+*/
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
